4fb4ad9a105c0b83e3c64caa0c2ad110c288c4ed
jphung <jphung@zingsoft.com>
2021-06-07 10:28:14 -0700

    [BugFix] Fix TDF for properties relating to plot and legend

--- projects/zingchart-angular/src/zingchart.d.ts
+++ projects/zingchart-angular/src/zingchart.d.ts
@@ -3341,6 +3341,10 @@ export interface graphset {
             */
            'callout-width'?: any;
            /**
+            * Sets the style of the cursor when hovering over a node. "hand" | "normal"
+            */
+            cursor?: string;
+           /**
             * Sets the angle of the axis along which the linear gradient is drawn. -45 | 115 | ...
             */
            'fill-angle'?: number;
@@ -3504,6 +3508,10 @@ export interface graphset {
             */
            'border-width'?: any;
            /**
+            * Sets the style of the cursor when hovering over a node. "hand" | "normal"
+            */
+           cursor?: string;
+           /**
             * Sets the angle of the axis along which the linear gradient is drawn. -45 | 115 | ...
             */
            'fill-angle'?: number;
@@ -3578,6 +3586,10 @@ export interface graphset {
             * Sets the distance between the shadow and the object. 4 | "6px" | ...
             */
            'shadow-distance'?: any;
+           /**
+            * Sets the size of the object/shape. 4 | "6px" | ...
+            */
+           size?: any;
            /**
             * Sets the visibility of the object. Allows you to turn off the object without removing lines of JSON. true | false | 1 | 0
             */
@@ -5177,6 +5189,10 @@ export interface graphset {
         */
        description?: string;
        /**
+        * Turns off click on slices
+        */
+       detached?: boolean;
+       /**
         * By default ZingChart uses sampling when rendering charts. This helps improve rendering speeds and typically does not effect the ap
         * pearance of the chart. However, using the attribute "exact": true within the "plot": { } object forces ZingChart to render all nod
         * es. true | false | 1 | 0
@@ -5409,6 +5425,10 @@ export interface graphset {
         * Bubble Charts and Bubble Pie Charts Only: Sets a multiplier (default 1) used to increase/decrease the bubble size 5 | 10 | ...
         */
        'size-factor'?: number;
+       /**
+        * Hole size in middle of chart
+        */
+       slice?: number;
        /**
         * Nested Pie Charts Only: Sets the initial offset of the pie layers when making a nestedpie 5 | 10 | ...
         */
@@ -7689,6 +7709,10 @@ export interface graphset {
             * Sets whether or not the object's shadow is visible. Has limited effect on HTML5 implementation. true | false | 1 | 0
             */
            shadow?: boolean;
+            /**
+             * Sets the text content of the object. "Some Text" | ...
+             */
+            text?: string;
            /**
             * Sets the transparency of the text. Values must range between 0.0 and 1.0, with 0.0 being completely invisible and 1.0 being comple
             * tely opaque. Please note that values also require the leading 0 before the decimal. 0.3 | 0.9 | ...
