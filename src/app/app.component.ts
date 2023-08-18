54685cb6e6b7631b2f0d67bcc2e70ceef8939fbd
Bram Goedvriend <bram.goedvriend@gmail.com>
2023-08-18 13:39:42 +0200

    Update packages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,48 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-
 export class AppComponent {
   interval: any;
 
-  title = 'zing-app';
-  series: ZingchartAngular.series = [{
-    alpha: 1,
-    values: [2,3,5,5],
-  }]
+  title = "zing-app";
+  series: ZingchartAngular.series = [
+    {
+      alpha: 1,
+      values: [2, 3, 5, 5],
+    },
+  ];
   shell: ZingchartAngular.graphset = {
-    type: 'area', 
-  }
-  config: ZingchartAngular.graphset = {
-    type: 'line',
-    series: [{
-      values: [3,4,5,5,6,7,5,3]
-    }]
+    type: "area",
   };
-  log=''
+  config: ZingchartAngular.graphset = {
+    type: "line",
+    series: [
+      {
+        values: [3, 4, 5, 5, 6, 7, 5, 3],
+      },
+    ],
+  };
+  log = "";
   onComplete(event) {
-    console.log('zingchart on complete fired!', event);
+    console.log("zingchart on complete fired!", event);
   }
 
   ngOnDestroy() {
     window.clearTimeout(this.interval);
   }
 
-  ngAfterContentInit()	{
+  ngAfterContentInit() {
     this.interval = setInterval(() => {
-      this.series = [{
-        alpha: 1,
-        values: [5,11,15,25].map(val => Math.floor(Math.random() * val)),
-      }]
-    }, 2000)
+      this.series = [
+        {
+          alpha: 1,
+          values: [5, 11, 15, 25].map((val) => Math.floor(Math.random() * val)),
+        },
+      ];
+    }, 2000);
   }
 }
